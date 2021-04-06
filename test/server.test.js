fb51ad2904a7bf4e9ee0d1579cd25673536aa2e1
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-04-07 00:53:09 +0700

    Increase the coverage

--- test/server.test.js
+++ test/server.test.js
@@ -41,6 +41,208 @@ test('success POST /books', async () => {
   expect(data.result.data.bookId).toBeTruthy();
 });
 
+test('success GET /books?reading=0', async () => {
+  const options = {
+    method: 'POST',
+    url: '/books',
+    payload: JSON.stringify({
+      name: "New Unique Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 1,
+      reading: false,
+    })
+  };
+  const data = await server.inject(options);
+  expect(data.statusCode).toBe(201);
+  expect(data.result.status).toBe('success');
+  expect(data.result.message).toBe('Buku berhasil ditambahkan');
+  const bookId = data.result.data.bookId;
+  expect(bookId).toBeTruthy();
+  const optionsUpdate = {
+    method: 'GET',
+    url: `/books?reading=0`,
+  };
+  const dataUpdate = await server.inject(optionsUpdate);
+  expect(dataUpdate.statusCode).toBe(200);
+  expect(dataUpdate.result.status).toBe('success');
+  expect(dataUpdate.result.data.books).toBeTruthy();
+  expect(dataUpdate.result.data.books.length).toBeGreaterThanOrEqual(1);
+});
+
+test('success GET /books?finished=1', async () => {
+  const options = {
+    method: 'POST',
+    url: '/books',
+    payload: JSON.stringify({
+      name: "New Finished Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 2,
+      reading: false,
+    })
+  };
+  const data = await server.inject(options);
+  expect(data.statusCode).toBe(201);
+  expect(data.result.status).toBe('success');
+  expect(data.result.message).toBe('Buku berhasil ditambahkan');
+  const bookId = data.result.data.bookId;
+  expect(bookId).toBeTruthy();
+  const optionsUpdate = {
+    method: 'GET',
+    url: `/books?finished=1`,
+  };
+  const dataUpdate = await server.inject(optionsUpdate);
+  expect(dataUpdate.statusCode).toBe(200);
+  expect(dataUpdate.result.status).toBe('success');
+  expect(dataUpdate.result.data.books).toBeTruthy();
+  expect(dataUpdate.result.data.books.length).toBeGreaterThanOrEqual(1);
+});
+
+test('success GET /books?name=jiahaha', async () => {
+  const options = {
+    method: 'POST',
+    url: '/books',
+    payload: JSON.stringify({
+      name: "New jiahaha Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 1,
+      reading: false,
+    })
+  };
+  const data = await server.inject(options);
+  expect(data.statusCode).toBe(201);
+  expect(data.result.status).toBe('success');
+  expect(data.result.message).toBe('Buku berhasil ditambahkan');
+  const bookId = data.result.data.bookId;
+  expect(bookId).toBeTruthy();
+  const optionsUpdate = {
+    method: 'GET',
+    url: `/books?name=jiahaha`,
+  };
+  const dataUpdate = await server.inject(optionsUpdate);
+  expect(dataUpdate.statusCode).toBe(200);
+  expect(dataUpdate.result.status).toBe('success');
+  expect(dataUpdate.result.data.books).toBeTruthy();
+  expect(dataUpdate.result.data.books.length).toBeGreaterThanOrEqual(1);
+});
+
+test('success PUT /books/{id}', async () => {
+  const options = {
+    method: 'POST',
+    url: '/books',
+    payload: JSON.stringify({
+      name: "New Unique Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 1,
+      reading: false,
+    })
+  };
+  const data = await server.inject(options);
+  expect(data.statusCode).toBe(201);
+  expect(data.result.status).toBe('success');
+  expect(data.result.message).toBe('Buku berhasil ditambahkan');
+  const bookId = data.result.data.bookId;
+  expect(bookId).toBeTruthy();
+  const optionsUpdate = {
+    method: 'PUT',
+    url: `/books/${bookId}`,
+    payload: JSON.stringify({
+      name: "New Update Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 1,
+      reading: false,
+    })
+  };
+  const dataUpdate = await server.inject(optionsUpdate);
+  expect(dataUpdate.statusCode).toBe(200);
+  expect(dataUpdate.result.status).toBe('success');
+  expect(dataUpdate.result.message).toBe('Buku berhasil diperbarui');
+});
+
+
+test('success DELETE /books/{id}', async () => {
+  const options = {
+    method: 'POST',
+    url: '/books',
+    payload: JSON.stringify({
+      name: "New Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 1,
+      reading: true,
+    })
+  };
+  const data = await server.inject(options);
+  expect(data.statusCode).toBe(201);
+  expect(data.result.status).toBe('success');
+  expect(data.result.message).toBe('Buku berhasil ditambahkan');
+  const bookId = data.result.data.bookId;
+  expect(bookId).toBeTruthy();
+  const optionsUpdate = {
+    method: 'DELETE',
+    url: `/books/${bookId}`,
+  };
+  const dataUpdate = await server.inject(optionsUpdate);
+  expect(dataUpdate.statusCode).toBe(200);
+  expect(dataUpdate.result.status).toBe('success');
+  expect(dataUpdate.result.message).toBe('Buku berhasil dihapus');
+});
+
+test('success GET /books/{id}', async () => {
+  const options = {
+    method: 'POST',
+    url: '/books',
+    payload: JSON.stringify({
+      name: "New Book",
+      year: 2020,
+      author: "Berv",
+      summary: "Great book",
+      publisher: "Berv Project",
+      pageCount: 2,
+      readPage: 1,
+      reading: true,
+    })
+  };
+  const data = await server.inject(options);
+  expect(data.statusCode).toBe(201);
+  expect(data.result.status).toBe('success');
+  expect(data.result.message).toBe('Buku berhasil ditambahkan');
+  const bookId = data.result.data.bookId;
+  expect(bookId).toBeTruthy();
+  const optionsUpdate = {
+    method: 'GET',
+    url: `/books/${bookId}`,
+  };
+  const dataUpdate = await server.inject(optionsUpdate);
+  expect(dataUpdate.statusCode).toBe(200);
+  expect(dataUpdate.result.status).toBe('success');
+  expect(dataUpdate.result.data.book).toBeTruthy();
+});
+
+
+
 test('failed POST /books without name', async () => {
   const options = {
     method: 'POST',
