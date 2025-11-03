ba152c660003613d7f02887e30d373b8b598a324
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-11-03 10:53:40 +0700

    fix: fix parsing

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/(?!uuid)',
+  ]
 };
